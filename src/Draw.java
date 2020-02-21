fe3be9e330ad2df0b5646bbf6e6a26b3c707c24c
Tomasz Stepnik <tomasz.stepnik77@gmail.com>
2020-02-21 15:13:41 +0100

    add try/catch and exit option,also final Strings

--- src/Draw.java
+++ src/Draw.java
@@ -1,39 +1,59 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Draw {
     Scanner sc = new Scanner(System.in);
 
+    public final static String RECTANGLE_NAME = "rectangle";
+    public final static String SQUARE_NAME = "square";
+    public final static String TREE_NAME = "tree";
+    public final static String EXIT = "exit";
+
     public void chooseAndDraw() {
+        boolean error = true;
+        while (error) {
+            try {
+                String choose = "";
+                while (!choose.equals(EXIT)) {
 
-        System.out.println("Co chciałbyś wydrukować?");
-        System.out.println("tree/rectangle/square");
-        String choose = sc.nextLine();
+                    System.out.println("Co chciałbyś wydrukować?");
+                    System.out.println("tree/rectangle/square");
+                    System.out.println("wyjście z gry, wpisz- " + EXIT);
+                    choose = sc.nextLine();
+                    switch (choose) {
+                        case TREE_NAME:
+                            System.out.println("Podaj wysokość choinki:");
+                            int height = nextInt();
+                            Tree tree = new Tree(height);
+                            tree.draw();
+                            break;
+                        case RECTANGLE_NAME:
+                            System.out.println("Podaj długość pierwszego boku:");
+                            int sideA = nextInt();
+                            System.out.println("Podaj długość drugiego boku:");
+                            int sideB = nextInt();
+                            Rectangle rectangle = new Rectangle(sideA, sideB);
+                            rectangle.draw();
+                            break;
+                        case SQUARE_NAME:
+                            System.out.println("Podaj długość boku:");
+                            int side = nextInt();
+                            Square square = new Square(side);
+                            break;
+                        case EXIT:
+                            System.out.println("Zakończenie programu.");
+                        default:
+                            System.out.println("Możesz wpisywać tylko słowa podane w instrukcji.");
+                    }
+                }
 
-        switch (choose) {
-            case "tree":
-                System.out.println("Podaj wysokość choinki:");
-                int height = nextInt();
-                Tree tree = new Tree(height);
-                tree.draw();
-                break;
-            case "rectangle":
-                System.out.println("Podaj długość pierwszego boku:");
-                int sideA = nextInt();
-                System.out.println("Podaj długość drugiego boku:");
-                int sideB = nextInt();
-                Rectangle rectangle = new Rectangle(sideA,sideB);
-                rectangle.draw();
-                break;
-            case "square":
-                System.out.println("Podaj długość boku:");
-                int side = nextInt();
-                Square square = new Square(side);
-                break;
-            case "exit":
-                System.out.println("Zakończenie programu.");
-            default:
-                System.out.println("Podałeś niepoprawne dane.");
+                error = false;
 
+            } catch (InputMismatchException i) {
+                System.out.println("Podawaj tylko znaki zgodne z instrukcją, nie wpisuj liczb tam gdzie wymagana" +
+                        " jest liczba i odwrotnie.");
+                sc.nextLine();
+            }
         }
     }
 
